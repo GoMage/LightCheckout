6df671e1e181a5dea47c6ba4fcec887d6a236b36
Andrey Zatserklyaniy <az@stepcart.com>
2014-02-18 16:12:01 +0200

    gomage-6065

--- app/code/local/GoMage/Checkout/Block/Form/Element/Geoip.php
+++ app/code/local/GoMage/Checkout/Block/Form/Element/Geoip.php
@@ -1,5 +1,5 @@
 <?php
- /**
+/**
  * GoMage LightCheckout Extension
  *
  * @category     Extension
@@ -9,26 +9,20 @@
  * @terms of use http://www.gomage.com/terms-of-use
  * @version      Release: 5.0
  * @since        Class available since Release 2.0
- */ 
-	
-	class GoMage_Checkout_Block_Form_Element_Geoip extends Mage_Adminhtml_Block_System_Config_Form_Field {
-		
-		protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element){
-    		
-    		if(!file_exists(Mage::getBaseDir('media')."/geoip/GeoLiteCity.dat")){
-    		
-    			$element->setDisabled(true);
-    			
-    			if($element->getId() == 'gomage_checkout_geoip_geoip_enabled'){
-	    		
-	    			$element->setComment($this->__('To use GeoIP you need to upload GeoliteCity.dat file to folder /media/geoip. Read more in the <a target="_blank" href="%s">Installation Guide</a>', 'http://www.gomage.com/media/extensions/lightcheckout/GoMage_LightCheckout_Installation_Guide.pdf'));
-	    		
-	    		}
-    			
-    		}
+ */
 
-    		return parent::_getElementHtml($element);
-    		
-    	}
-		
-	}
+class GoMage_Checkout_Block_Form_Element_Geoip extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {
+        if (!file_exists(Mage::getBaseDir('media') . "/geoip/GeoLiteCity.dat")) {
+            $element->setDisabled(true);
+            if ($element->getId() == 'gomage_checkout_geoip_geoip_enabled') {
+                $element->setComment($this->__('To use GeoIP you need to upload GeoliteCity.dat file to folder /media/geoip. Read more in the <a target="_blank" href="%s">Installation Guide</a>', 'http://wiki.gomage.com/display/lc/GeoIP+Installation'));
+            }
+        }
+        return parent::_getElementHtml($element);
+    }
+
+}
