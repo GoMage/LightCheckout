c2563802718a10b99a1f0db729e66d01c729aaa3
Andrey Zatserklyaniy <az@stepcart.com>
2014-02-24 14:49:05 +0200

    gomage-4868

--- app/code/local/GoMage/Checkout/Block/Config/Block.php
+++ app/code/local/GoMage/Checkout/Block/Config/Block.php
@@ -11,48 +11,56 @@
  * @since        Class available since Release 4.0
  */
 
-class GoMage_Checkout_Block_Config_Block extends Mage_Core_Block_Template {
-	
-	protected $config_block;
-	protected $ifconfig;
-	protected $call_methods = array();
-	
-	protected function _toHtml() {
-		if ($this->config_block) {
-			if ($this->ifconfig && ($configPath = ( string ) $this->ifconfig)) {
-				if (! Mage::getStoreConfigFlag($configPath)) {
-					return null;
-				}
-			}
-			try {
-				$block = $this->getLayout()->createBlock($this->config_block);
-				foreach ($this->call_methods as $method) {
-					call_user_func_array(array($block, $method['method']), $method['params']);
-				}
-			}
-			catch (Exception $e) {
-				$block = null;
-			}
-			if ($block) {
-				return $block->toHtml();
-			}
-		}
-		return null;
-	}
-	
-	public function setConfigBlock($config_block) {
-		$this->config_block = $config_block;
-		return $this;
-	}
-	
-	public function setIfConfig($ifconfig) {
-		$this->ifconfig = $ifconfig;
-		return $this;
-	}
-	
-	public function setCallMethod($method, $parms = array()) {
-		$this->call_methods[] = array('method' => $method, 'params' => $parms);
-		return $this;
-	}
+class GoMage_Checkout_Block_Config_Block extends Mage_Core_Block_Template
+{
+
+    protected $config_block;
+    protected $ifconfig;
+    protected $call_methods = array();
+
+    protected function _toHtml()
+    {
+        if ($this->config_block) {
+            if ($this->ifconfig && ($configPath = ( string )$this->ifconfig)) {
+                if (!Mage::getStoreConfigFlag($configPath)) {
+                    return null;
+                }
+            }
+            try {
+                $block = $this->getLayout()->createBlock($this->config_block);
+                foreach ($this->call_methods as $method) {
+                    call_user_func_array(array($block, $method['method']), $method['params']);
+                }
+                foreach ($this->getSortedChildren() as $child) {
+                    $child_block = $this->getLayout()->getBlock($child);
+                    $block->append($child_block, $child_block->getBlockAlias());
+                }
+            } catch (Exception $e) {
+                $block = null;
+            }
+            if ($block) {
+                return $block->toHtml();
+            }
+        }
+        return null;
+    }
+
+    public function setConfigBlock($config_block)
+    {
+        $this->config_block = $config_block;
+        return $this;
+    }
+
+    public function setIfConfig($ifconfig)
+    {
+        $this->ifconfig = $ifconfig;
+        return $this;
+    }
+
+    public function setCallMethod($method, $parms = array())
+    {
+        $this->call_methods[] = array('method' => $method, 'params' => $parms);
+        return $this;
+    }
 
 }
