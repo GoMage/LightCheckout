076368f40b5fe800a52e973372c9e358cc233de2
Andrey Zatserklyaniy <az@stepcart.com>
2014-03-05 13:17:43 +0200

    Copyright

--- app/code/local/GoMage/Checkout/Model/Quote/Address/Total/Shipping.php
+++ app/code/local/GoMage/Checkout/Model/Quote/Address/Total/Shipping.php
@@ -5,11 +5,11 @@
  * GoMage LightCheckout Extension
  *
  * @category     Extension
- * @copyright    Copyright (c) 2010-2013 GoMage.com (http://www.gomage.com)
+ * @copyright    Copyright (c) 2010-2014 GoMage.com (http://www.gomage.com)
  * @author       GoMage.com
  * @license      http://www.gomage.com/licensing  Single domain license
  * @terms of use http://www.gomage.com/terms-of-use
- * @version      Release: 5.0
+ * @version      Release: 5.3
  * @since        Class available since Release 2.4
  */
 
